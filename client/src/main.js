0c14188bda8dc9801205a21f46b053e8b129af8a
doktordirk <doktordirk@outlook.de>
2016-01-17 09:02:23 +0100

    chore(project) dependency update feat(client): Use Endpioints

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,12 @@ import 'bootstrap';
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
-
+    .developmentLogging()
+    .plugin('spoonx/aurelia-api', config => {
+      config
+        .registerEndpoint('api', 'http://localhost:3000/api/')
+        .setDefaultEndpoint('api');
+    });
   //Uncomment the line below to enable animation.
   aurelia.use.plugin('aurelia-animator-css');
   //if the css animator is enabled, add swap-order="after" to all router-view elements
--- client/src/modules/customer/customerData.js
+++ client/src/modules/customer/customerData.js
@@ -1,13 +1,13 @@
 import {inject} from 'aurelia-framework';
-import {Rest} from 'SpoonX/aurelia-api';
+import {Endpoint} from 'spoonx/aurelia-api';
 
-@inject(Rest)
+@inject(Endpoint.of())
 export class CustomerData {
 
   model = 'customers';
 
-  constructor(rest) {
-    this.rest = rest;
+  constructor(apiEndpoint) {
+    this.apiEndpoint = apiEndpoint;
   }
 
   get modelPath() {
@@ -15,25 +15,25 @@ export class CustomerData {
   }
 
   getById(id) {
-    return this.rest.find(this.modelPath, id); 
+    return this.apiEndpoint.find(this.modelPath, id); 
   }
   
   getAll() {
-    return this.rest.find(this.modelPath);  
+    return this.apiEndpoint.find(this.modelPath);  
   }
 
   delete(customer) {
-    return this.rest.destroy(this.modelPath, customer.id)
+    return this.apiEndpoint.destroy(this.modelPath, customer.id)
   }
 
   save(customer) {
     let request;
 
     if (customer.id) {
-      request = this.rest.update(this.modelPath, customer.id, customer);
+      request = this.apiEndpoint.update(this.modelPath, customer.id, customer);
     }
     else {
-      request = this.rest.create(this.modelPath, customer);
+      request = this.apiEndpoint.create(this.modelPath, customer);
     }
 
     return request;
